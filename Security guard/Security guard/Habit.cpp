496cedd30e46688fe4ff75d969635799178152e2
Slava <157728760+xParik@users.noreply.github.com>
2024-11-19 20:26:56 +0300

    Revert "Habit class"
    This reverts commit 350c4e7eaabe064d46d422a93e9b1439ddcf6091.

--- Security guard/Security guard/Habit.cpp
+++ Security guard/Security guard/Habit.cpp
@@ -1,16 +1,1 @@
 #include "Habit.h"
-using namespace std;
-
-Habit::Habit(const std::string& name, const std::string& description, SDL_Texture* texture, int healthEffect, int staminaEffect) : name(name), description(description), texture(texture), healthEffect(healthEffect), staminaEffect(staminaEffect) {}
-Habit::~Habit() {
-    if (texture != nullptr) {
-        SDL_DestroyTexture(texture);
-        texture = nullptr;
-    }
-}
-void Habit::render(SDL_Renderer* renderer, int x, int y) {
-    if (texture != nullptr) {
-        SDL_Rect dstRect = { x, y, 1280, 720 };
-        SDL_RenderCopy(renderer, texture, nullptr, &dstRect);
-    }
-}
--- Security guard/Security guard/Habit.h
+++ Security guard/Security guard/Habit.h
@@ -1,20 +1,5 @@
 #pragma once
-#include <string>
-#include "SDL.h"
-using namespace std;
-
 class Habit
 {
-private:
-	string name;
-	string description;
-	int healthEffect;
-	int staminaEffect;
-	SDL_Texture* texture;
-public:
-	Habit(const string& name, const string& description, SDL_Texture* texture, int healthEffect, int staminaEffect);
-	~Habit();
-	void render(SDL_Renderer* renderer, int x, int y);
-	string getName() const;
-	string Effect(int& health, int& stamina) const;
 };
+
