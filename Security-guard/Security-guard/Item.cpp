ad8df177c232ad44e1256c4283e2508d7da5174d
Slava <157728760+xParik@users.noreply.github.com>
2024-12-12 19:24:29 +0300

    Item

--- Security-guard/Security-guard/Item.cpp
+++ Security-guard/Security-guard/Item.cpp
@@ -1,1 +1,10 @@
 #include "Item.h"
+#include <iostream>
+#include <string>
+
+Item::Item(const string& name, const string& description,
+    const string* allowedEffects, int numAllowedEffects,
+    const string* prohibitedEffects, int numProhibitedEffects) :
+    name(name), description(description),
+    numAllowedEffects(numAllowedEffects), numProhibitedEffects(numProhibitedEffects) {}
+
